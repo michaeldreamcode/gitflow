a0fd97dce536ca5416c6bfb331134fc6c73660f6
michael <michael.munoz@dreamcodesoft.com>
2020-10-20 12:22:03 -0500

    se crea paquete cn clase main

---
+++ src/main/java/com/dreamcode/gitflow/main.java
@@ -0,0 +1,9 @@
+package com.dreamcode.gitflow;
+
+public class main {
+
+    public static void main(String[] args) {
+            System.out.println("inicio de trabajo feature_develop");
+    }
+
+}
